fdd64a259b0332b8ad0694377d4ed9503304d4fc
Jessica <jessica.weeber@gmail.com>
2016-04-11 16:51:58 -0700

    added load method to go through csv and add them to the table

--- lib/setup_db.rb
+++ lib/setup_db.rb
@@ -1,7 +1,12 @@
 require 'sqlite3'
+require 'csv'
 
 class ReleaseDatabase
+
+  FILE_PATH = 'source/20160406-record-collection.csv'
+
   attr_reader :db
+
   def initialize(dbname = "releases")
     @db = SQLite3::Database.new "database/#{dbname}.db"
   end
@@ -23,7 +28,26 @@ class ReleaseDatabase
     @db.execute("DROP TABLE IF EXISTS albums;")
     @db.execute(query)
   end
+
+  def load!
+     # query that is designed for re-use
+     insert_statement = <<-INSERTSTATEMENT
+      INSERT INTO albums (
+        label_code, artist, title, label, format, released, date_added
+      ) VALUES (
+        :label_code, :artist, :title, :label, :format, :released, :date_added
+      );
+     INSERTSTATEMENT
+
+     prepared_statement = @db.prepare(insert_statement)
+
+     # iterate through csv now that we have prepared statement, so values will populate our database
+     CSV.foreach(FILE_PATH, headers: true) do |row|
+      prepared_statement.execute(row.to_h)
+     end
+  end
 end
 
 release_db = ReleaseDatabase.new
 release_db.reset_schema!
+release_db.load!
