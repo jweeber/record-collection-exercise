3ca095b624e386646b9d38b89d4d846ec53f7e34
Jessica <jessica.weeber@gmail.com>
2016-04-11 16:19:01 -0700

    set up class, reset method and executed command to create table

---
+++ lib/setup_db.rb
@@ -0,0 +1,29 @@
+require 'sqlite3'
+
+class ReleaseDatabase
+  attr_reader :db
+  def initialize(dbname = "releases")
+    @db = SQLite3::Database.new "database/#{dbname}.db"
+  end
+
+  def reset_schema!
+    query = <<-CREATESTATEMENT
+    CREATE TABLE albums (
+    id INTEGER PRIMARY KEY,
+    label_code BLOB,
+    artist TEXT NOT NULL,
+    title TEXT NOT NULL,
+    label TEXT,
+    format TEXT,
+    released INTEGER,
+    date_added TEXT
+    );
+    CREATESTATEMENT
+    # execute only works for one query
+    @db.execute("DROP TABLE IF EXISTS albums;")
+    @db.execute(query)
+  end
+end
+
+release_db = ReleaseDatabase.new
+release_db.reset_schema!
