c634245782a6ca5eb0997b93c9862bd6786c535b
Jessica <jessica.weeber@gmail.com>
2016-04-12 08:53:22 -0700

    added jeremy's new method and messages for setting up the database

--- lib/setup_db.rb
+++ lib/setup_db.rb
@@ -2,7 +2,6 @@ require 'sqlite3'
 require 'csv'
 
 class ReleaseDatabase
-
   FILE_PATH = 'source/20160406-record-collection.csv'
 
   attr_reader :db
@@ -11,43 +10,58 @@ class ReleaseDatabase
     @db = SQLite3::Database.new "database/#{dbname}.db"
   end
 
+  def setup!
+    reset_schema!
+    load!
+  end
+
+  private
+
   def reset_schema!
+    puts "Recreating Schema..."
+
+    # table name: albums
+    # label_code,artist,title,label,format,released,date_added
+    # BLOB, TEXT, TEXT, TEXT, TEXT,        INTEGER,     TEXT
+    # one table
     query = <<-CREATESTATEMENT
-    CREATE TABLE albums (
-    id INTEGER PRIMARY KEY,
-    label_code BLOB,
-    artist TEXT NOT NULL,
-    title TEXT NOT NULL,
-    label TEXT,
-    format TEXT,
-    released INTEGER,
-    date_added TEXT
-    );
+      CREATE TABLE albums (
+        id INTEGER PRIMARY KEY,
+        label_code BLOB,
+        artist TEXT NOT NULL,
+        title TEXT NOT NULL,
+        label TEXT,
+        format TEXT,
+        released INTEGER,
+        date_added TEXT
+      );
     CREATESTATEMENT
-    # execute only works for one query
-    @db.execute("DROP TABLE IF EXISTS albums;")
-    @db.execute(query)
+
+    db.execute("DROP TABLE IF EXISTS albums;")
+    db.execute(query) #runs one and only one query!
   end
 
   def load!
-     # query that is designed for re-use
-     insert_statement = <<-INSERTSTATEMENT
+    puts "Preparing INSERT statements..."
+
+    insert_statement = <<-INSERTSTATEMENT
       INSERT INTO albums (
         label_code, artist, title, label, format, released, date_added
       ) VALUES (
         :label_code, :artist, :title, :label, :format, :released, :date_added
       );
-     INSERTSTATEMENT
+    INSERTSTATEMENT
 
-     prepared_statement = @db.prepare(insert_statement)
+    prepared_statement = db.prepare(insert_statement)
 
-     # iterate through csv now that we have prepared statement, so values will populate our database
-     CSV.foreach(FILE_PATH, headers: true) do |row|
+    # now that we have a prepared statement...
+    # let's iterate the csv and use its values to populate our database
+    CSV.foreach(FILE_PATH, headers: true) do |row|
       prepared_statement.execute(row.to_h)
-     end
+    end
+
+    puts "Data import complete!"
   end
 end
 
-release_db = ReleaseDatabase.new
-release_db.reset_schema!
-release_db.load!
+ReleaseDatabase.new.setup!
